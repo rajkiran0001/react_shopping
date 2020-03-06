407d2f948eae389838f525227d07c9274461ebe4
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-03-06 10:50:38 +0100

    updated functionalities

---
+++ src/components/Cart.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Cart extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Hello from Cart</h3>
+            </div>
+        )
+    }
+}
+
+export default Cart
---
+++ src/components/Default.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class Default extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Page not found</h3>
+                
+            </div>
+        )
+    }
+}
+
+export default Default

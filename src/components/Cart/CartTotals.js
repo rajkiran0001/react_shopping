230aab5f51f364ff17bdf699a43067be67111315
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-03-25 18:53:53 +0100

    added paypal button

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import PayPalButton from './PayPalButton'
 
-function CartTotals({value}) {
-    const {cartSubTotal, cartTax, cartTotal,clearCart} = value;
+
+function CartTotals({ value, history }) {
+    const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
     console.log(value);
-    
+
     return (
         <>
             <div className="container">
                 <div className="row">
                     <div className="col-20 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
                         <Link to="/">
-                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5" onClick={()=> clearCart()}>clear cart</button>
+                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5" onClick={() => clearCart()}>clear cart</button>
                         </Link>
                         <h5>
                             <span className="text-title">subtotal :</span>
@@ -25,6 +27,8 @@ function CartTotals({value}) {
                             <span className="text-title">total :</span>
                             <stron>$ {cartTotal}</stron>
                         </h5>
+                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
+
                     </div>
                 </div>
             </div>
